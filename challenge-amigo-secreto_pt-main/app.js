8870271cc3b7c01b8f9f1b2528f996a3be9b3229
Leonardo Farias Souzam <lfsouza20@gmail.com>
2025-08-19 07:23:07 -0300

    Implementação da lógica de funcionamento do sorteio do amigo secreto.

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,9 +1,94 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.Funcionalidades:
+// declaração de variáveis globais
+let amigos = [];
+let amigosDisponiveis = [];
+let listaDeAmigos = document.getElementById("listaAmigos");
+let nomeSorteado = document.getElementById("resultado");
 
-/*Adicionar nomes: Os usuários escreverão o nome de um amigo em um campo de texto e o adicionarão a uma lista visível ao clicar em "Adicionar".
+// aciona a função adicionarAmigo apertando enter
+document.getElementById("amigo").addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        adicionarAmigo();
+    }
+});
 
-Validar entrada: Se o campo de texto estiver vazio, o programa exibirá um alerta solicitando um nome válido.
+// função para adicionar amigo
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById("amigo");
 
-Visualizar a lista: Os nomes inseridos aparecerão em uma lista abaixo do campo de entrada.
+    if (nomeAmigo.value.trim() === '') {
+        alert("Por favor, insira um nome válido.")
+    } else {
+        amigos.push(nomeAmigo.value.trim());
+        amigosDisponiveis = amigos.slice();
+        nomeAmigo.value = '';
+        atualizarListaAmigos();
+    }
 
-Sorteio aleatório: Ao clicar no botão "Sortear Amigo", um nome da lista será selecionado aleatoriamente e exibido na página.*/
+}
+
+// função para atualizar lista de amigos
+function atualizarListaAmigos() {
+
+    listaDeAmigos.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++) {
+        listaDeAmigos.innerHTML += `<li> ${amigos[i]} </li>`;
+    }
+
+}
+
+// função para sortear o amigo secreto
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert(' Adicione pelo menos 2 amigos para começar.');
+        return;
+    }
+   
+
+    if (amigosDisponiveis.length === 0 ) {
+
+        alert('Todos os amigos já foram sorteados. Reiniciando o sorteio.');
+        amigosDisponiveis = amigos.slice();
+        
+    } 
+
+    // esconde a lista de amigos na tela
+    listaDeAmigos.style.display = 'none';
+
+    let indiceAleatorio = Math.floor(Math.random() * amigosDisponiveis.length);
+
+    //inicia o display com o nome e a contagem regressiva
+    nomeSorteado.innerHTML = `
+        <h3> O amigo secreto sorteado é: </h3>
+        <p class = "nome-sorteado">${amigosDisponiveis[indiceAleatorio]}</p>
+        <p class="contagem-regressiva"></p>
+        `;
+
+    // Inicia o cronômetro
+    iniciarContagemRegressiva(2);
+
+    //remove o nome sorteado do array
+    amigosDisponiveis.splice(indiceAleatorio, 1);
+    console.log(amigos);
+    console.log(amigosDisponiveis);
+
+}
+
+
+function iniciarContagemRegressiva(tempo) {
+    const timer = setInterval(() => {
+        if (tempo > 0) {
+            nomeSorteado.querySelector('.contagem-regressiva').textContent = `Você tem ${tempo} segundos para anotar.`;
+            tempo--;
+        } else {
+            clearInterval(timer);
+            nomeSorteado.innerHTML = '';
+            listaDeAmigos.style.display = 'block';
+            atualizarListaAmigos();
+        }
+    }, 1000);
+
+}
+
+
+
+
